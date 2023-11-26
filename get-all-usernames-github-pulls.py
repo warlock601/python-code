1d6856eb3e327a5ec24a03309779faed253fcb7b
Vivek Raj <vr32288@gmail.com>
2023-11-26 19:09:59 +0530

    Update get-all-usernames-github-pulls.py

--- get-all-usernames-github-pulls.py
+++ get-all-usernames-github-pulls.py
@@ -1,12 +1,12 @@
 # to get this, first we need to have "requests" library installed "pip install requests" 
 # we'll use this library to get the API requests 
 # we can get the github pulls API url from the Github API docs ie /repos/{owner}/{repo}/pulls
-
+# to get list of all usernames we ran for loop for the complete pulls page 
 
 import requests
 get_url = requests.get("https://api.github.com/repos/kubernetes/kubernetes/pulls")       # owner=kubernetes,repo=kubernetes 
 
-detail=get_url.json()                                                                    # we'll get this in a json format in a variable
+detail = get_url.json()                                                                  # we'll get this in a json format in a variable
 
 for i in range(len(detail)):                                                             # detail[0]- gives first record in dictionary 
     print(detail[i]["user"]["login"])                                                    # detail[0]["id"] - gives the id of the first record in dictionary
