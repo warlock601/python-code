a9178afd08d93fa0f5b63c4d2a6abb5f497e87e2
Vivek Raj <vr32288@gmail.com>
2023-11-30 08:34:03 +0530

    added comments for readability

--- change parameters for configuration file using file operations/updatre_file.py
+++ change parameters for configuration file using file operations/updatre_file.py
@@ -1,11 +1,15 @@
+# opne() is used to open a file, takes 2 parameters-- File path & "r" or "W" for read or Write respectively
+# algo for this will be ----    first read the file > write all the contents onto a variable > open the file with write >  change the parameter
+
+
 def update_file(file_path,key,value):
-  with open(file_path,"r") as file:
-    lines=file.readlines()
+  with open(file_path,"r") as file:                # Read everything in the given file & created "file" as alias 
+    lines=file.readlines()                         # Used readlines() to read each & every line & stored whatever we read in a variable "lines"
 
   with open(file_path,"w") as file:
-    for line in lines:
+    for line in lines:                             # to iterate line by line
       if key in line:
-        file.write(key + "=" + value)
+        file.write(key + "=" + value)              # if key is found, replace the value of that key by new value
       else:
         file.write(line)
 
