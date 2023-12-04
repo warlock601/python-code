23c126b701f588e626694cbd8c731da03d535b60
Vivek Raj <vr32288@gmail.com>
2023-12-04 21:11:52 +0530

    Update ebs_volumes.py
    created a list instead of printing volumes one after the other

--- ebs_volumes.py
+++ ebs_volumes.py
@@ -1,12 +1,15 @@
-# This prog is to get all the avaiable volumes in a spoecific aws region in your account using boto3 module
+# This prog is to get the list of all avaiable volumes in a spoecific aws region in your account using boto3 module
 # we needed access and secret access key as we're trying from the local IDE, if we're doing it inside Lambda function in aws, then we don't need keys
 # In boto3.client(), we'll need to mention keys and region as well
 
 import boto3
 
-aws_access_key =  'your aws access key'
-aws_secret_key = 'your aws secret access key'
-aws_region = 'region in which you wanna find volumes'
+aws_access_key =  'AKIARZ5D4LNQPATS367I'
+aws_secret_key = 'czo8w/q2zBo3dSTrX2mVn/1NO8gS/fDtY3P8nngm'
+aws_region = 'us-east-1'
+
+volume_all=[]
+
 
 ec2_client = boto3.client('ec2',aws_access_key_id=aws_access_key,aws_secret_access_key=aws_secret_key,region_name=aws_region)
 
@@ -14,8 +17,9 @@ response=ec2_client.describe_volumes()
 
 for volume in response['Volumes']:
     volume_id = volume['VolumeId']
-        #volume_all.append(volume_id)   
+    volume_all.append(volume_id)   
 
-    print(volume_id)
+print(volume_all)
+
 
 # In the same way, we can create progs for getting EC2 instances, snapshots and other resources etc
